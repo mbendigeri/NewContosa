1ff34f1b77a396062ce3db35f4c382bfab4924c8
Mallikarjun Sharanabasappa Bendigeri <mbendigeri@gmail.com>
2017-05-01 23:35:33 +0530

    Add latest files
    Add latest files

--- NewContosoUniversity/wwwroot/lib/UICalendar/MyApp.js
+++ NewContosoUniversity/wwwroot/lib/UICalendar/MyApp.js
@@ -1,37 +1,50 @@
 ﻿var app = angular.module('myApp', ['ui.calendar']);
-app.controller('myNgController', ['$scope', '$http', 'uiCalendarConfig', function ($scope, $http, uiCalendarConfig) {
+app.controller('myNgController', ['$scope', '$http', 'uiCalendarConfig', function ($scope, $http, uiCalendarConfig)
+{
     
     $scope.SelectedEvent = null;
     var isFirstTime = true;
 
     $scope.events = [];
     $scope.eventSources = [$scope.events];
-
-
+    
+    
     //Load events from server
-    $http.get('/home/getevents', {
-        cache: true,
-        params: {}
-    }).then(function (data) {
-        $scope.events.slice(0, $scope.events.length);
-        angular.forEach(data.data, function (value) {
-            $scope.events.push({
-                title: value.Title,
-                description: value.Description,
-                start: new Date(parseInt(value.StartAt.substr(6))),
-                end: new Date(parseInt(value.EndAt.substr(6))),
-                allDay: value.IsFullDay,
-                stick: true
+    function MakehttpCall(Sid)
+    {
+       
+        $http.get('/CustomerSupport/GetF2FMeetings', {
+            cache: false,
+            params: { staffID: Sid }
+        }).then(function (data) {
+            $scope.events.slice(0, $scope.events.length);
+            angular.forEach(data.data, function (value) {
+                $scope.events.push({
+                    title: value.course.courseTitle,
+                    description: "Meeting with " + value.interaction.customer.firstName + "," + value.interaction.customer.lastName,
+                    start: new Date(value.faceToFaceMeetingDateTime),
+                    end: CalculateEndTime(value.faceToFaceMeetingDateTime, value.allotedTimeInMinutes),//new Date(value.FaceToFaceMeetingDateTime)
+                    allDay: value.IsFullDay,
+                    stick: true
+                });
             });
         });
-    });
+    }
+    function CalculateEndTime(strdate,minToadd)
+    {
+        var d = new Date(strdate);
+        f = new Date(d.getTime());
+        d.setMinutes(d.getMinutes() + parseInt(minToadd));
+        return d;
 
+    }
+    //uiCalendarConfig.calendars.myCalendar.fullCalendar('gotoDate', now);
     //configure calendar
     $scope.uiConfig = {
         calendar: {
             height: 450,
             editable: true,
-            displayEventTime: false,
+            displayEventTime: true,
             header: {
                 left: 'month basicWeek basicDay agendaWeek agendaDay',
                 center: 'title',
@@ -49,5 +62,27 @@ app.controller('myNgController', ['$scope', '$http', 'uiCalendarConfig', functio
             }
         }
     };
+
+
+    $("#F2FMeetingDate").on("dp.change",
+        function (e)
+        {
+            //$('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+        uiCalendarConfig.calendars.myCalendar.fullCalendar('gotoDate', e.date._d);
+        }
+    );
+
+
+    $(function(){
+
+        $("#SelectedStaffID").change(function() {
+
+            var t = $(this).val();
+            $scope.events.splice(0, $scope.events.length)
+            MakehttpCall(t);
+            
+
+        });
+    });
 
 }])
